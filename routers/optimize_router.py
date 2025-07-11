2000d5f4056859a04f7746d2c70b73d8efc03f97
Sayantansen123 <riosayan123@gmail.com>
2025-07-11 17:37:02 +0530

    updated

--- routers/optimize_router.py
+++ routers/optimize_router.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from dto.optimize_prompt_dto import PromptRequest
 
 optimize_router = APIRouter(tags="Prompt Optimize")
 
+
+@optimize_router.post("/optimize")
+def optimize_prompt()
